1eaf8323437065280eb30b9c2914e2d30812d319
Dominik <horakdo1@fjfi.cvut.cz>
2024-04-15 18:07:14 +0200

    Macro update

--- include/lbm3d/d3q27/macro.h
+++ include/lbm3d/d3q27/macro.h
@@ -36,7 +36,7 @@ struct D3Q27_MACRO_Default : D3Q27_MACRO_Base< TRAITS >
 	using dreal = typename TRAITS::dreal;
 	using idx = typename TRAITS::idx;
 
-	enum { e_rho, e_vx, e_vy, e_vz, N, e_vmx, e_vmy, e_vmz };
+	enum { e_rho, e_vx, e_vy, e_vz, e_vmx, e_vmy, e_vmz, N };
 
 	template < typename LBM_DATA, typename LBM_KS >
 	CUDA_HOSTDEV static void outputMacro(LBM_DATA &SD, LBM_KS &KS, idx x, idx y, idx z)
