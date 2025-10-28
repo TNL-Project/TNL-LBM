54a38aa317c07fb3e3513375e0a1e89cd85bd399
MarekOnd <ondrejtablet@gmail.com>
2025-10-28 10:55:51 +0100

    fixed equilibrium for D3Q343 second order hermite

--- include/lbm3d/d3q343/eq.h
+++ include/lbm3d/d3q343/eq.h
@@ -11,15 +11,16 @@ struct D3Q343_EQ
 	using dreal = typename TRAITS::dreal;
 
 	static constexpr const dreal T0 = (dreal)0.6979533220196830882384091; // from closure 105T^3 − 210T^2 + 147T − 36 = 0 in D1Q7
+	static constexpr const dreal n1oT0 = 1./T0;
     static constexpr const dreal w0 = (dreal)(1.0*(36-49*T0+42*T0*T0-15*T0*T0*T0)/36);
     static constexpr const dreal w1 = (dreal)(1.0*T0*(12-13*T0+5*T0*T0)/16);
     static constexpr const dreal w2 = (dreal)(1.0*T0*(-3+10*T0-5*T0*T0)/40);
     static constexpr const dreal w3 = (dreal)(1.0*T0*(4-15*T0+15*T0*T0)/720);
 
 	__cuda_callable__ static dreal feq(dreal rho, int qx, int qy, int qz, dreal vx, dreal vy, dreal vz){
-		dreal ws[4] = {w0,w1,w2,w3};
-		const dreal eq = no1 - n3o2 * (vx * vx + vy * vy + vz * vz) + no3 * (qx * vx + qy * vy + qz * vz)
-		+ n9o2 * (qx * vx + qy * vy + qz * vz) * (qx * vx + qy * vy + qz * vz);
+		const dreal ws[4] = {w0,w1,w2,w3};
+		const dreal eq = no1 - n1o2 * n1oT0 * (vx * vx + vy * vy + vz * vz) + n1oT0 * (qx * vx + qy * vy + qz * vz)
+		+ n1o2 * n1oT0 * n1oT0 * (qx * vx + qy * vy + qz * vz) * (qx * vx + qy * vy + qz * vz);
 		return ws[abs(qx)]*ws[abs(qy)]*ws[abs(qz)]*rho*eq;
 	}
 };
