0ed5c896e1761e4bd242af553368319c63867305
MarekOnd <ondrejtablet@gmail.com>
2025-11-09 10:57:22 +0100

    added draft of nasa bump in channel simulation

---
+++ sim_NSE/sim_bump_NASA.cu
@@ -0,0 +1,222 @@
+#include <argparse/argparse.hpp>
+#include <utility>
+
+// As of now, enum and sync direction are specific for different models and need to be included before core!!!
+//#include "lbm3d/d3q27/defs.h"
+#include "lbm3d/d3q343/defs.h"
+#include "lbm3d/core.h"
+
+template <typename NSE>
+struct StateLocal : State<NSE>
+{
+	using TRAITS = typename NSE::TRAITS;
+	using BC = typename NSE::BC;
+	using MACRO = typename NSE::MACRO;
+	using BLOCK = LBM_BLOCK<NSE>;
+
+	using State<NSE>::nse;
+	using State<NSE>::vtk_helper;
+
+	using idx = typename TRAITS::idx;
+	using real = typename TRAITS::real;
+	using dreal = typename TRAITS::dreal;
+	using point_t = typename TRAITS::point_t;
+	using lat_t = Lattice<3, real, idx>;
+
+	real lbm_inflow_vx = 0;
+
+	void setupBoundaries() override
+	{
+		nse.setBoundaryX(0, BC::GEO_INFLOW);						  // left
+		nse.setBoundaryX(1, BC::GEO_INFLOW);						  // left
+		nse.setBoundaryX(2, BC::GEO_INFLOW);						  // left
+		nse.setBoundaryX(nse.lat.global.x() - 1, BC::GEO_OUTFLOW_RIGHT);  // right
+		nse.setBoundaryX(nse.lat.global.x() - 2, BC::GEO_OUTFLOW_RIGHT);  // right
+		nse.setBoundaryX(nse.lat.global.x() - 3, BC::GEO_OUTFLOW_RIGHT);  // right
+
+		//nse.setBoundaryX(0,                      BC::GEO_PERIODIC);						  // left
+		//nse.setBoundaryX(1,                      BC::GEO_PERIODIC);						  // left
+		//nse.setBoundaryX(2,                      BC::GEO_PERIODIC);						  // left
+		//nse.setBoundaryX(nse.lat.global.x() - 1, BC::GEO_PERIODIC);  // right
+		//nse.setBoundaryX(nse.lat.global.x() - 2, BC::GEO_PERIODIC);  // right
+		//nse.setBoundaryX(nse.lat.global.x() - 3, BC::GEO_PERIODIC);  // right
+
+		const int margin = 0;
+		nse.setBoundaryZ(1-1+margin,                      BC::GEO_WALL);						 // top
+		nse.setBoundaryZ(2-1+margin,                      BC::GEO_WALL);						 // top
+		nse.setBoundaryZ(3-1+margin,                      BC::GEO_WALL);						 // top
+		nse.setBoundaryZ(nse.lat.global.z() - 2+1-margin, BC::GEO_WALL);	 // bottom
+		nse.setBoundaryZ(nse.lat.global.z() - 3+1-margin, BC::GEO_WALL);	 // bottom
+		nse.setBoundaryZ(nse.lat.global.z() - 4+1-margin, BC::GEO_WALL);	 // bottom
+		nse.setBoundaryY(1-1+margin, 					  BC::GEO_WALL);						 // back
+		nse.setBoundaryY(2-1+margin, 					  BC::GEO_WALL);						 // back
+		nse.setBoundaryY(3-1+margin, 					  BC::GEO_WALL);						 // back
+		nse.setBoundaryY(nse.lat.global.y() - 2+1-margin, BC::GEO_WALL);	 // front
+		nse.setBoundaryY(nse.lat.global.y() - 3+1-margin, BC::GEO_WALL);	 // front
+		nse.setBoundaryY(nse.lat.global.y() - 4+1-margin, BC::GEO_WALL);	 // front
+
+		for (int px = 0; px <= nse.lat.global.x(); px++){
+		for (int pz = 0; pz <= nse.lat.global.y(); pz++){
+		for (int py = 0; py <= nse.lat.global.z(); py++){
+			float x = nse.lat.lbm2physX(px);
+			float y = nse.lat.lbm2physY(py);
+			float z = nse.lat.lbm2physZ(pz);
+			// Shift the x for non-symmetric bump
+			float xshift = x + pow(sin(PI*y),4)
+
+			// Bump area
+			if(xshift > 0.3 && xshift  < 1.5){
+				float fxy = 0.05*pow(sin(PI*xshift/0.9 - PI/3.),4) // Wall position
+				if(fxy > z){
+					nse.setMap(px, py, pz, BC::GEO_WALL);
+				}
+			}
+		}}}
+		// TODO: set the symmetric walls
+	}
+
+
+	bool outputData(const BLOCK& block, int index, int dof, char* desc, idx x, idx y, idx z, real& value, int& dofs) override
+	{
+		int k = 0;
+		if (index == k++)
+			return vtk_helper("lbm_density", block.hmacro(MACRO::e_rho, x, y, z), 1, desc, value, dofs);
+		if (index == k++) {
+			switch (dof) {
+				case 0:
+					return vtk_helper("velocity", block.hmacro(MACRO::e_vx, x, y, z), 3, desc, value, dofs);
+				case 1:
+					return vtk_helper("velocity", block.hmacro(MACRO::e_vy, x, y, z), 3, desc, value, dofs);
+				case 2:
+					return vtk_helper("velocity", block.hmacro(MACRO::e_vz, x, y, z), 3, desc, value, dofs);
+			}
+		}
+		return false;
+	}
+
+	void updateKernelVelocities() override
+	{
+		for (auto& block : nse.blocks) {
+			block.data.inflow_vx = lbm_inflow_vx;
+			block.data.inflow_vy = 0;
+			block.data.inflow_vz = 0;
+		}
+	}
+
+	StateLocal(const std::string& id, const TNL::MPI::Comm& communicator, lat_t lat)
+	: State<NSE>(id, communicator, std::move(lat))
+	{}
+};
+
+template <typename NSE>
+int sim(int RESOLUTION = 2)
+{
+	using idx = typename NSE::TRAITS::idx;
+	using real = typename NSE::TRAITS::real;
+	using point_t = typename NSE::TRAITS::point_t;
+	using lat_t = Lattice<3, real, idx>;
+
+	int block_size = 32;
+	real PHYS_LENGTH = 50.; // length in some units (NASA does not specify)
+	real PHYS_HEIGHT = 5.;		  // domain height (physical)
+	real PHYS_DEPTH = 0.5;		  // domain depth (physical)
+	// TODO: solve the rounding of pixels to have it precise
+	int X = floor(PHYS_LENGTH * RESOLUTION);  // width in pixels
+	int Y = floor(PHYS_DEPTH * RESOLUTION);  // height in pixels --- top and bottom walls NoDV px
+	int Z = floor(PHYS_HEIGHT * RESOLUTION); // depth in pixels --- top and bottom walls  NoDV px
+	real LBM_VISCOSITY = 0.0001;
+	real PHYS_VISCOSITY = 1.5e-2;
+	real PHYS_VELOCITY = 1.0;
+	real PHYS_DL = PHYS_HEIGHT / ((real) Z - 6); // naive fullway bounce-back
+	real PHYS_DT = LBM_VISCOSITY / PHYS_VISCOSITY * PHYS_DL * PHYS_DL;	//PHYS_HEIGHT/(real)LBM_HEIGHT;
+	point_t PHYS_ORIGIN = {-25., -0.5, 0.};
+
+	// initialize the lattice
+	lat_t lat;
+	lat.global = typename lat_t::CoordinatesType(X, Y, Z);
+	lat.physOrigin = PHYS_ORIGIN;
+	lat.physDl = PHYS_DL;
+	lat.physDt = PHYS_DT;
+	lat.physViscosity = PHYS_VISCOSITY;
+
+	const std::string state_id = fmt::format("sim_bump_NASA_res{:02d}_np{:03d}", RESOLUTION, TNL::MPI::GetSize(MPI_COMM_WORLD));
+	StateLocal<NSE> state(state_id, MPI_COMM_WORLD, lat);
+
+	// problem parameters
+	state.lbm_inflow_vx = lat.phys2lbmVelocity(PHYS_VELOCITY);
+
+	state.nse.physFinalTime = 1.0;
+	state.cnt[PRINT].period = 0.001;
+
+	// add cuts
+	state.cnt[VTK2D].period = 0.001;
+	state.add2Dcut_X(X / 2, "cutsX/cut_X");
+	state.add2Dcut_Y(Y / 2, "cutsY/cut_Y");
+	state.add2Dcut_Z(Z / 2, "cutsZ/cut_Z");
+
+	state.cnt[VTK3D].period = 0.1;
+	state.cnt[VTK3DCUT].period = 0.1;
+	state.add3Dcut(X / 4, Y / 4, Z / 4, X / 2, Y / 2, Z / 2, 2, "box");
+
+	execute(state);
+
+	return 0;
+}
+
+template <typename TRAITS = TraitsSP>
+void run(int RES)
+{
+	// D3Q27
+	//using COLL = D3Q27_CUM<TRAITS, D3Q27_EQ_INV_CUM<TRAITS>>;
+	//using NSE_CONFIG = LBM_CONFIG<
+	//	TRAITS,
+	//	D3Q27_KernelStruct,
+	//	NSE_Data_ConstInflow<TRAITS>,
+	//	COLL,
+	//	typename COLL::EQ,
+	//	D3Q27_STREAMING<TRAITS>,
+	//	D3Q27_BC_All,
+	//	D3Q27_MACRO_Default<TRAITS>>;
+
+	// D3Q343
+	using COLL = D3Q343_SRT<TRAITS, D3Q343_EQ<TRAITS>>;
+	using NSE_CONFIG = LBM_CONFIG<
+		TRAITS,
+		D3Q343_KernelStruct,
+		NSE_Data_ConstInflow<TRAITS>,
+		COLL,
+		typename COLL::EQ,
+		D3Q343_STREAMING<TRAITS>,
+		D3Q343_BC_All,
+		D3Q343_MACRO_Default<TRAITS>>;
+
+	sim<NSE_CONFIG>(RES);
+}
+
+int main(int argc, char** argv)
+{
+	TNLMPI_INIT mpi(argc, argv);
+
+	argparse::ArgumentParser program("sim_bump_NASA");
+	program.add_description("Simulation of a bump in a channel.");
+	program.add_argument("resolution").help("resolution of the lattice").scan<'i', int>().default_value(1);
+
+	try {
+		program.parse_args(argc, argv);
+	}
+	catch (const std::exception& err) {
+		std::cerr << err.what() << '\n';
+		std::cerr << program;
+		return 1;
+	}
+
+	const auto resolution = program.get<int>("resolution");
+	if (resolution < 1) {
+		fmt::println(stderr, "CLI error: resolution must be at least 1");
+		return 1;
+	}
+
+	run(resolution);
+
+	return 0;
+}
